66d4f848a468665027fd7166ffc498bc4c905a72
ElmerAS <eialfaro29@gmail.com>
2018-06-20 18:06:21 -0500

    Repositorio iniciado - Base

---
+++ src/main/java/com/dekka/studiousmvc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dekka.studiousmvc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudiousMvcApplication.class);
+	}
+
+}
---
+++ src/main/java/com/dekka/studiousmvc/StudiousMvcApplication.java
@@ -0,0 +1,12 @@
+package com.dekka.studiousmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiousMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiousMvcApplication.class, args);
+	}
+}
